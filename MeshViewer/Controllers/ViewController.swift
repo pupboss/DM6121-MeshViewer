14bafb9a344d4e932170b674b8f19b9b58ab79ce
Jet Lee <pupboss.i@gmail.com>
2019-03-30 22:36:57 +0800

    Done

--- MeshViewer/Controllers/ViewController.swift
+++ MeshViewer/Controllers/ViewController.swift
@@ -14,44 +14,46 @@ class ViewController: UIViewController {
 
     lazy var scene: SCNScene = {
         let scene = SCNScene()
-        // camera
-        let cameraNode = SCNNode()
-        let camera = SCNCamera()
-        cameraNode.camera = camera
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 10)
-        scene.rootNode.addChildNode(cameraNode)
         
-//        let lightNode = SCNNode()
-//        lightNode.light = SCNLight()
-//        lightNode.light?.type = .omni
-//        lightNode.position = SCNVector3Make(0, 10, 10)
-//        scene.rootNode.addChildNode(lightNode)
-//
-//        let ambientLightNode = SCNNode()
-//        ambientLightNode.light = SCNLight()
-//        ambientLightNode.light?.type = .ambient
-//        ambientLightNode.light?.color = UIColor.darkGray
-//        scene.rootNode.addChildNode(ambientLightNode)
+        let floorNode = SCNNode()
+        let floor = SCNFloor()
+        floorNode.geometry = floor
+        floorNode.position = SCNVector3(0, -3, 0)
+        let floorPhysicsBody = SCNPhysicsBody(type: SCNPhysicsBodyType.static, shape: SCNPhysicsShape(geometry: floor, options: nil))
+        floorNode.physicsBody = floorPhysicsBody
+        scene.rootNode.addChildNode(floorNode)
         
         // Create sources
         let vertexSource = SCNGeometrySource(vertices: threeDModel.vertices)
         let normalSource = SCNGeometrySource(normals: threeDModel.normals)
-        
+
         // Create element
-        let solidElement = SCNGeometryElement(indices: threeDModel.faces, primitiveType: .triangles)
-        
+        let solidElement = SCNGeometryElement(indices: threeDModel.faces, primitiveType: .point)
+
         // Create geometry
         let geometry = SCNGeometry(sources: [vertexSource, normalSource], elements: [solidElement])
-        
+
         // Set materials
         let solidMaterial = SCNMaterial()
         solidMaterial.diffuse.contents = UIColor.tc.theme
-        solidMaterial.locksAmbientWithDiffuse = true
-        
+//        solidMaterial.locksAmbientWithDiffuse = true
+
         geometry.materials = [solidMaterial]
-        
+
         let cubeNode = SCNNode(geometry: geometry)
         scene.rootNode.addChildNode(cubeNode)
+
+        // camera
+        let cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        cameraNode.position = SCNVector3(x: 0, y: 0, z: 2)
+        scene.rootNode.addChildNode(cameraNode)
+        
+        let lightNode = SCNNode()
+        lightNode.light = SCNLight()
+        lightNode.light?.type = .omni
+        lightNode.position = SCNVector3Make(0, 3, 3)
+        scene.rootNode.addChildNode(lightNode)
         
         return scene
     }()
@@ -61,11 +63,12 @@ class ViewController: UIViewController {
         threeDView.scene = scene
         threeDView.backgroundColor = UIColor.tc.background
         threeDView.allowsCameraControl = true
+        threeDView.showsStatistics = true
         return threeDView
     }()
     
     lazy var threeDModel: ThreeDModel = {
-        return Utils.parseThreeDModel(fromFile: "gargoyle")
+        return Utils.parseThreeDModel(fromFile: "bunny")
     }()
     
     override func viewDidLoad() {
